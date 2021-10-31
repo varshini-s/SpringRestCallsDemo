7df4e3317b057ed967b616f599430ac3cb862ee3
varshini <dreamvarshini@gmail.com>
2021-10-31 13:17:18 +0530

    ADDED: function to demonstrate get request

---
+++ src/main/java/com/bridgelabz/restcallsdemo/controller/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.restcallsdemo.controller;
+
+import com.bridgelabz.restcallsdemo.model.User;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController 
+{
+    @RequestMapping(value = {"","/","/home"})
+    public String sayHello()
+    {
+        return "Hello from bridgelabz!";
+    }
+
+
+}
