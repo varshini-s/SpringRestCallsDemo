acd256b0f88bb45a1b1a60114cfd9724db100702
varshini <dreamvarshini@gmail.com>
2021-10-31 13:28:33 +0530

    ADDED: function to demonstrate put request method

--- src/main/java/com/bridgelabz/restcallsdemo/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/restcallsdemo/controller/HelloWorldController.java
@@ -43,4 +43,10 @@ public class HelloWorldController
         return "Hello "+user.getFirstName()+" "+user.getLastName();
     }
 	
+    @PutMapping("put/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam(value="lastName") String lastName) {
+        
+        return "Hello "+firstName+" "+lastName+"!";
+    }
+
 }
