1f41f1d7ee85966dd4b4772e7f85cee50952584e
varshini <dreamvarshini@gmail.com>
2021-10-31 13:24:44 +0530

    ADDED: main method for the application

---
+++ src/main/java/com/bridgelabz/restcallsdemo/RestCallsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.restcallsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCallsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCallsDemoApplication.class, args);
+	}
+
+}
